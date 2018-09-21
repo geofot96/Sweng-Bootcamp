45c063b1611e8d1a4462089bf3f130ec75140d9b
Georgios Fotiadis <georgios.fotiadis@epfl.ch>
2018-09-21 16:51:09 +0200

    Added behavior to the app

--- app/src/main/java/ch/epfl/sweng/bootcamp/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/bootcamp/MainActivity.java
@@ -1,13 +1,27 @@
 package ch.epfl.sweng.bootcamp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "STRING";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void greetings(View view)
+    {
+        Intent intent = new Intent(this, GreetingActivity.class);
+        EditText editText =(EditText) findViewById(R.id.mainName);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
